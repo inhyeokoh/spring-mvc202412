14fd83005fdb188447e0458373091817e1b76f94
soongu <naong526@naver.com>
2024-12-23 15:22:08 +0900

    성적 정보 리포지토리 클래스 구현

--- src/main/java/com/spring/mvcproject/score/api/ScoreApiController.java
+++ src/main/java/com/spring/mvcproject/score/api/ScoreApiController.java
@@ -8,15 +8,12 @@ import com.spring.mvcproject.score.service.ScoreService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.*;
-import java.util.stream.Collectors;
-
-import static java.util.Comparator.*;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestController // JSON응답
 @RequestMapping("/api/v1/scores")
