3ebdf9b57ee38a49803ae60d3287d2798a8c836a
soongu <naong526@naver.com>
2024-12-30 14:56:32 +0900

    Chap06 - 예외처리 : 공통예외처리 실무 표준 사례

--- src/main/java/com/spring/mvcproject/chap6_3/api/MemberApiController.java
+++ src/main/java/com/spring/mvcproject/chap6_3/api/MemberApiController.java
@@ -1,18 +1,21 @@
 package com.spring.mvcproject.chap6_3.api;
 
+import com.spring.mvcproject.chap6_3.dto.request.MemberCreateRequest;
 import com.spring.mvcproject.chap6_3.entity.Member;
+import com.spring.mvcproject.chap6_3.exception.MemberException;
+import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v6/members")
+@Slf4j  // 로그 라이브러리
 public class MemberApiController {
 
     private Map<String, Member> memberStore = new HashMap<>();
@@ -38,15 +41,36 @@ public class MemberApiController {
     @GetMapping("/{account}")
     public ResponseEntity<?> findOne(@PathVariable String account) {
 
+        log.info("회원 단일 조회 요청이 들어옴!");
+        log.debug("parameter account : {}", account);
+
         if (account.length() > 10) {
-            throw new IllegalStateException("계정명은 10글자 이내여야 합니다.");
+//            log.warn("사용자 입력값 오류!!");
+            throw new MemberException(HttpStatus.BAD_REQUEST, "계정명은 10글자 이내여야 합니다.");
         }
 
         Member member = memberStore.get(account);
 
         if (member == null) {
-            throw new IllegalStateException("회원 계정명을 다시 확인하세요!");
+//            log.error("심각한 오류 발생!");
+            throw new MemberException(HttpStatus.NOT_FOUND, "회원 계정명을 다시 확인하세요!");
         }
+
+        return ResponseEntity.ok().body(member);
+    }
+
+    @PostMapping
+    public ResponseEntity<?> create(
+            @RequestBody @Valid MemberCreateRequest dto
+    ) {
+
+        Member member = Member.builder()
+                .nickname(dto.getNickname())
+                .account(dto.getAccount())
+                .password(dto.getPassword())
+                .build();
+
+        memberStore.put(member.getAccount(), member);
 
         return ResponseEntity.ok().body(member);
     }
